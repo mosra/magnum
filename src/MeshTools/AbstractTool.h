79a36bd373c17597d9c55e433fb9c764696d4fcc
Vladimír Vondruš <mosra@centrum.cz>
2012-01-12 01:06:13 +0100

    New namespace and base class for mesh tools.

---
+++ src/MeshTools/AbstractTool.h
@@ -0,0 +1,54 @@
+#ifndef Magnum_MeshTools_AbstractTool_h
+#define Magnum_MeshTools_AbstractTool_h
+/*
+    Copyright © 2010, 2011, 2012 Vladimír Vondruš <mosra@centrum.cz>
+
+    This file is part of Magnum.
+
+    Magnum is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    Magnum is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+/** @file
+ * @brief Class Magnum::MeshTools::AbstractTool
+ */
+
+#include "MeshBuilder.h"
+
+namespace Magnum {
+
+/**
+ * @brief %Mesh tools
+ *
+ * Tools for generating, optimizing and cleaning meshes.
+ */
+namespace MeshTools {
+
+/**
+ * @brief Base class for mesh tools
+ *
+ * Provides access to internals of MeshBuilder instance for subclasses.
+ */
+template<class Vertex> class AbstractTool {
+    public:
+        /**
+         * @brief Constructor
+         * @param builder   %Mesh builder to operate on
+         */
+        inline AbstractTool(MeshBuilder<Vertex>& builder): builder(builder), vertices(builder._vertices), indices(builder._indices) {}
+
+    protected:
+        MeshBuilder<Vertex>& builder;       /**< @brief Builder instance */
+        std::vector<Vertex>& vertices;      /**< @brief Builder vertices */
+        std::vector<unsigned int>& indices; /**< @brief Builder indices */
+};
+
+}}
+
+#endif
