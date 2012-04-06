32a73395e233de41ce19c052dc34a2c450b700a8
Vladimír Vondruš <mosra@centrum.cz>
2012-04-06 02:09:51 +0200

    Plane primitive.

---
+++ src/Primitives/Plane.h
@@ -0,0 +1,39 @@
+#ifndef Magnum_Primitives_Plane_h
+#define Magnum_Primitives_Plane_h
+/*
+    Copyright © 2010, 2011, 2012 Vladimír Vondruš <mosra@centrum.cz>
+
+    This file is part of Magnum.
+
+    Magnum is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    Magnum is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+/** @file
+ * @brief Class Magnum::Primitives::Plane
+ */
+
+#include "Trade/MeshData.h"
+
+namespace Magnum { namespace Primitives {
+
+/**
+@brief %Plane primitive
+
+2x2 plane orthogonal to Z axis.
+*/
+class Plane: public Trade::MeshData {
+    public:
+        /** @brief Constructor */
+        Plane();
+};
+
+}}
+
+#endif
