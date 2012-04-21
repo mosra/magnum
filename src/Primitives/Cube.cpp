3a96420cb48ad059c0f0c1c3b17735a3bb40d9fe
Vladimír Vondruš <mosra@centrum.cz>
2012-04-22 00:47:44 +0200

    Flipped cube primitive face winding, so the faces are oriented outside.

--- src/Primitives/Cube.cpp
+++ src/Primitives/Cube.cpp
@@ -20,18 +20,18 @@ using namespace std;
 namespace Magnum { namespace Primitives {
 
 Cube::Cube(): MeshData(Mesh::Primitive::Triangles, new vector<unsigned int>{
-    0, 1, 2,
-    2, 1, 3,
-    1, 5, 3,
-    3, 5, 7,
-    5, 4, 7,
-    7, 4, 6,
-    4, 0, 6,
-    6, 0, 2,
-    2, 3, 7,
-    2, 7, 6,
-    4, 5, 1,
-    4, 1, 0
+    0, 2, 1,
+    2, 3, 1,
+    1, 3, 5,
+    3, 7, 5,
+    5, 7, 4,
+    7, 6, 4,
+    4, 6, 0,
+    6, 2, 0,
+    2, 7, 3,
+    2, 6, 7,
+    4, 1, 5,
+    4, 0, 1
 }, {new vector<Vector4>}, {new vector<Vector3>{
     {-1.0f, -1.0f, -1.0f},
     { 1.0f, -1.0f, -1.0f},
