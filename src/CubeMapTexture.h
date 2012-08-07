6c244eb70374fbd2d6b45ba40a8bee072ed4b5af
Vladimír Vondruš <mosra@centrum.cz>
2012-08-07 16:01:54 +0200

    CubeMapTexture is always seamless on OpenGL ES 3.0.

--- src/CubeMapTexture.h
+++ src/CubeMapTexture.h
@@ -63,8 +63,10 @@ class CubeMapTexture: public AbstractTexture {
         /**
          * @brief Enable/disable seamless cube map textures
          *
+         * Initially disabled on desktop OpenGL.
          * @see @fn_gl{Enable}/@fn_gl{Disable} with @def_gl{TEXTURE_CUBE_MAP_SEAMLESS}
-         * @requires_gl
+         * @requires_gl Not available in OpenGL ES 2.0, always enabled in
+         *      OpenGL ES 3.0.
          * @requires_gl32 Extension @extension{ARB,seamless_cube_map}
          */
         inline static void setSeamless(bool enabled) {
