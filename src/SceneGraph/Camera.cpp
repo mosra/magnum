5c80fb0ad9181ad3af43f6d7bca92f7e780f72f8
Vladimír Vondruš <mosra@centrum.cz>
2012-08-21 11:28:02 +0200

    GCC 4.5 compatibility: cannot use initializer list here.

--- src/SceneGraph/Camera.cpp
+++ src/SceneGraph/Camera.cpp
@@ -49,7 +49,7 @@ template Matrix4 aspectRatioFix<Matrix4>(AspectRatioPolicy, const Vector2&, cons
 template<class MatrixType, class VectorType, class ObjectType, class SceneType, class CameraType> Camera<MatrixType, VectorType, ObjectType, SceneType, CameraType>::Camera(ObjectType* parent): ObjectType(parent), _aspectRatioPolicy(AspectRatioPolicy::NotPreserved) {}
 
 template<class MatrixType, class VectorType, class ObjectType, class SceneType, class CameraType> void Camera<MatrixType, VectorType, ObjectType, SceneType, CameraType>::setViewport(const Math::Vector2<GLsizei>& size) {
-    Framebuffer::setViewport({0, 0}, size);
+    Framebuffer::setViewport(Math::Vector2<GLint>{0, 0}, size);
 
     _viewport = size;
     fixAspectRatio();
