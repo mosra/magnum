4f4e1230d7cc8d3ee491eed5eeba19f60f2fc8b7
Vladimír Vondruš <mosra@centrum.cz>
2013-03-30 19:23:48 +0100

    DebugTools: added support for 2D spheres into ShapeRenderer.

---
+++ src/DebugTools/Implementation/SphereRenderer.h
@@ -0,0 +1,54 @@
+#ifndef Magnum_DebugTools_Implementation_SphereRenderer_h
+#define Magnum_DebugTools_Implementation_SphereRenderer_h
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013 Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include "AbstractShapeRenderer.h"
+
+#include "Physics/Physics.h"
+
+#include "corradeCompatibility.h"
+
+namespace Magnum { namespace DebugTools { namespace Implementation {
+
+template<UnsignedInt dimensions> class AbstractSphereRenderer;
+
+template<> class AbstractSphereRenderer<2>: public AbstractShapeRenderer<2> {
+    public:
+        AbstractSphereRenderer();
+};
+
+template<UnsignedInt dimensions> class SphereRenderer: public AbstractSphereRenderer<dimensions> {
+    public:
+        SphereRenderer(Physics::Sphere<dimensions>& sphere);
+
+        void draw(Resource<ShapeRendererOptions>& options, const typename DimensionTraits<dimensions>::MatrixType& projectionMatrix) override;
+
+    private:
+        Physics::Sphere<dimensions>& sphere;
+};
+
+}}}
+
+#endif
