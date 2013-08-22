bbcfe211a6c6aeef3200069c45cf198cea8623d2
Vladimír Vondruš <mosra@centrum.cz>
2013-08-22 16:00:55 +0200

    GCC 4.4 compatibility: linker error with defaulted destructor.
    Again.

--- src/SceneGraph/AbstractCamera.hpp
+++ src/SceneGraph/AbstractCamera.hpp
@@ -72,7 +72,8 @@ template<UnsignedInt dimensions, class T> AbstractCamera<dimensions, T>::Abstrac
     AbstractFeature<dimensions, T>::setCachedTransformations(CachedTransformation::InvertedAbsolute);
 }
 
-template<UnsignedInt dimensions, class T> AbstractCamera<dimensions, T>::~AbstractCamera() = default;
+/* `= default` causes linker errors in GCC 4.4 */
+template<UnsignedInt dimensions, class T> AbstractCamera<dimensions, T>::~AbstractCamera() {}
 
 template<UnsignedInt dimensions, class T> AbstractCamera<dimensions, T>& AbstractCamera<dimensions, T>::setAspectRatioPolicy(AspectRatioPolicy policy) {
     _aspectRatioPolicy = policy;
