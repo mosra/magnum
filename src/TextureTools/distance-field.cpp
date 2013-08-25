9f64e86584f2bcab9b31304b2b61e0007e4136f5
Vladimír Vondruš <mosra@centrum.cz>
2013-08-26 01:14:35 +0200

    TextureTools: added `magnum-distancefield` utility.
    Standalone command-line executable allowing for easy conversion of
    black&white images to distance field texture.

---
+++ src/TextureTools/distance-field.cpp
@@ -0,0 +1,134 @@
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013 Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include <Utility/Arguments.h>
+#include <PluginManager/Manager.h>
+
+#include "Math/Geometry/Rectangle.h"
+#include "Image.h"
+#include "ImageFormat.h"
+#include "Renderer.h"
+#include "Texture.h"
+#include "TextureFormat.h"
+#include "Platform/WindowlessGlxApplication.h"
+#include "TextureTools/DistanceField.h"
+#include "Trade/AbstractImporter.h"
+#include "Trade/AbstractImageConverter.h"
+#include "Trade/ImageData.h"
+
+#include "configure.h"
+
+namespace Magnum {
+
+class DistanceFieldConverter: public Platform::WindowlessApplication {
+    public:
+        explicit DistanceFieldConverter(const Arguments& arguments);
+
+        int exec() override;
+
+    private:
+        Utility::Arguments args;
+};
+
+DistanceFieldConverter::DistanceFieldConverter(const Arguments& arguments): WindowlessGlxApplication(arguments, nullptr) {
+    args.addArgument("input").setHelp("input", "input image")
+        .addArgument("output").setHelp("output", "output image")
+        .addOption("importer", "TgaImporter").setHelp("image importer plugin")
+        .addOption("converter", "TgaImageConverter").setHelp("image converter plugin")
+        .addNamedArgument("output-size").setHelpKey("output-size", "\"X Y\"").setHelp("output-size", "size of output image")
+        .addNamedArgument("radius").setHelpKey("radius", "N").setHelp("radius", "distance field computation radius")
+        .setHelp("Converts black&white image to distance-field representation.")
+        .parse(arguments.argc, arguments.argv);
+
+    createContext({});
+}
+
+int DistanceFieldConverter::exec() {
+    /* Load plugins */
+    PluginManager::Manager<Trade::AbstractImporter> importerManager(MAGNUM_IMPORTER_PLUGIN_DIR);
+    if(!(importerManager.load(args.value("importer")) & (PluginManager::LoadState::Loaded|PluginManager::LoadState::Static))) {
+        Error() << "Cannot load importer plugin" << args.value("importer") << "from" << MAGNUM_IMPORTER_PLUGIN_DIR;
+        return 1;
+    }
+    PluginManager::Manager<Trade::AbstractImageConverter> converterManager(MAGNUM_IMAGECONVERTER_PLUGIN_DIR);
+    if(!(converterManager.load(args.value("converter")) & (PluginManager::LoadState::Loaded|PluginManager::LoadState::Static))) {
+        Error() << "Cannot load converter plugin" << args.value("converter") << "from" << MAGNUM_IMAGECONVERTER_PLUGIN_DIR;
+        return 1;
+    }
+
+    /* Instance plugins */
+    Trade::AbstractImporter* importer = importerManager.instance(args.value("importer"));
+    Trade::AbstractImageConverter* converter = converterManager.instance(args.value("converter"));
+    CORRADE_INTERNAL_ASSERT(importer && converter);
+
+    /* Open input file */
+    Trade::ImageData2D* image = nullptr;
+    if(!importer->openFile(args.value("input")) || !(image = importer->image2D(0))) {
+        Error() << "Cannot open file" << args.value("input");
+        delete importer;
+        delete converter;
+        return 1;
+    }
+
+    delete importer;
+    if(image->format() != ImageFormat::Red) {
+        Error() << "Unsupported image format" << image->format();
+        delete converter;
+        return 1;
+    }
+
+    /* Input texture */
+    Texture2D input;
+    input.setMinificationFilter(Sampler::Filter::Linear)
+        .setMagnificationFilter(Sampler::Filter::Linear)
+        .setWrapping(Sampler::Wrapping::ClampToEdge)
+        .setImage(0, TextureFormat::R8, *image);
+
+    /* Output texture */
+    Texture2D output;
+    output.setStorage(1, TextureFormat::R8, args.value<Vector2i>("output-size"));
+
+    CORRADE_INTERNAL_ASSERT(Renderer::error() == Renderer::Error::NoError);
+
+    /* Do it */
+    Debug() << "Converting image of size" << image->size() << "to distance field...";
+    TextureTools::distanceField(input, output, {{}, args.value<Vector2i>("output-size")}, args.value<Int>("radius"), image->size());
+    delete image;
+
+    /* Save image */
+    Image2D result(ImageFormat::Red, ImageType::UnsignedByte);
+    output.image(0, result);
+    if(!converter->exportToFile(result, args.value("output"))) {
+        Error() << "Cannot save file" << args.value("output");
+        delete converter;
+        return 1;
+    }
+
+    delete converter;
+    return 0;
+}
+
+}
+
+MAGNUM_WINDOWLESSAPPLICATION_MAIN(Magnum::DistanceFieldConverter)
