c13f8a77f584ab58285dface3f23b08276d1a1d6
Vladimír Vondruš <mosra@centrum.cz>
2013-09-19 01:48:28 +0200

    Shaders: test compilation of *Vector shaders.

---
+++ src/Shaders/Test/VectorTest.cpp
@@ -0,0 +1,55 @@
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013 Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include "Shaders/Vector.h"
+#include "Test/AbstractOpenGLTester.h"
+
+namespace Magnum { namespace Shaders { namespace Test {
+
+class VectorTest: public Magnum::Test::AbstractOpenGLTester {
+    public:
+        explicit VectorTest();
+
+        void compile2D();
+        void compile3D();
+};
+
+VectorTest::VectorTest() {
+    addTests({&VectorTest::compile2D,
+              &VectorTest::compile3D});
+}
+
+void VectorTest::compile2D() {
+    Shaders::Vector2D shader;
+    CORRADE_VERIFY(shader.validate().first);
+}
+
+void VectorTest::compile3D() {
+    Shaders::Vector3D shader;
+    CORRADE_VERIFY(shader.validate().first);
+}
+
+}}}
+
+CORRADE_TEST_MAIN(Magnum::Shaders::Test::VectorTest)
