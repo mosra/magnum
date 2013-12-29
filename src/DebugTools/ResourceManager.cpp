168807ed64d04c0e90940f721c99f6ce74be7ec1
Vladimír Vondruš <mosra@centrum.cz>
2013-12-29 19:40:27 +0100

    DebugTools: fix warning on MinGW.

--- src/DebugTools/ResourceManager.cpp
+++ src/DebugTools/ResourceManager.cpp
@@ -35,11 +35,11 @@
 
 namespace Magnum {
 
-#ifndef CORRADE_TARGET_WINDOWS
-template class ResourceManager<AbstractShaderProgram, Buffer, Mesh, MeshView, DebugTools::ForceRendererOptions, DebugTools::ObjectRendererOptions, DebugTools::ShapeRendererOptions>;
-#else
-template class MAGNUM_DEBUGTOOLS_EXPORT ResourceManager<AbstractShaderProgram, Buffer, Mesh, MeshView, DebugTools::ForceRendererOptions, DebugTools::ObjectRendererOptions, DebugTools::ShapeRendererOptions>;
+template class
+#if defined(CORRADE_TARGET_WINDOWS) && _MSC_VER
+MAGNUM_DEBUGTOOLS_EXPORT
 #endif
+ResourceManager<AbstractShaderProgram, Buffer, Mesh, MeshView, DebugTools::ForceRendererOptions, DebugTools::ObjectRendererOptions, DebugTools::ShapeRendererOptions>;
 
 namespace DebugTools {
 
