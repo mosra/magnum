7e1c93121c3db8ae2af8d49de3257021edd61cfb
Vladimír Vondruš <mosra@centrum.cz>
2014-01-19 17:28:23 +0100

    modules: updated FindCorrade.cmake from Corrade repository.

--- modules/FindCorrade.cmake
+++ modules/FindCorrade.cmake
@@ -97,7 +97,7 @@
 #
 #   This file is part of Corrade.
 #
-#   Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013
+#   Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014
 #             Vladimír Vondruš <mosra@centrum.cz>
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a
