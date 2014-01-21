152701fe158964f79dd53310e299be289aea9b2d
Vladimír Vondruš <mosra@centrum.cz>
2014-01-21 02:27:24 +0100

    MSVC 2013 compatibility: different attribute placement.

--- src/Magnum/DebugMessage.cpp
+++ src/Magnum/DebugMessage.cpp
@@ -36,10 +36,11 @@ namespace Magnum {
 
 namespace {
 
+void
 #ifdef CORRADE_TARGET_WINDOWS
 APIENTRY
 #endif
-void callbackWrapper(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+callbackWrapper(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
     Context::current()->state().debug->messageCallback(DebugMessage::Source(source), DebugMessage::Type(type), id, DebugMessage::Severity(severity), std::string(message, length), userParam);
 }
 
