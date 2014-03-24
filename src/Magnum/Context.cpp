8a011b7c4eef8dffd0a9c7c40e0cfe6a153522b6
Vladimír Vondruš <mosra@centrum.cz>
2014-03-24 12:40:23 +0100

    Use more general MAGNUM_TARGET_WEBGL instead of CORRADE_TARGET_EMSCRIPTEN.
    Only where it matters, though.

--- src/Magnum/Context.cpp
+++ src/Magnum/Context.cpp
@@ -314,7 +314,7 @@ Context::Context() {
         const std::string version = versionString();
         #ifndef MAGNUM_TARGET_GLES
         if(version.compare(0, 4, "2.1 ") == 0)
-        #elif defined(CORRADE_TARGET_EMSCRIPTEN)
+        #elif defined(MAGNUM_TARGET_WEBGL)
         if(version.find("WebGL 1") != std::string::npos)
         #else
         if(version.find("OpenGL ES 2.0") != std::string::npos)
