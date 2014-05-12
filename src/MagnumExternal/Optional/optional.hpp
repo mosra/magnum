14684b04250222aedfb70f33f11a8d25b1c719b5
Stefan Wasilewski <smw@tau7.com>
2014-05-11 21:45:41 -0400

    fix compile problem on g++ 4.8.2 on os x

--- src/MagnumExternal/Optional/optional.hpp
+++ src/MagnumExternal/Optional/optional.hpp
@@ -154,7 +154,7 @@ template <class T> inline constexpr typename std::remove_reference<T>::type&& co
   # elif defined __clang__ || defined __GNU_LIBRARY__
     __assert(expr, file, line);
   # elif defined __GNUC__
-    _assert(expr, file, line);
+    __assert(expr, file, line);
   # else
   #   error UNSUPPORTED COMPILER
   # endif
