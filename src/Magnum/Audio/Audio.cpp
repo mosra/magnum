mosra/magnum

commit c7b23a884fabea1298ac4e68d7fa4e8da6ab31a6

Be consistent and use our own platform definitions.

src/Magnum/Audio/Audio.cpp
@@ -33,7 +33,7 @@ namespace Magnum { namespace Audio {
 /* Verify types */
 static_assert(std::is_same<ALubyte, UnsignedByte>::value, "ALubyte is not the same as UnsignedByte");
 /** @todo Why `ALbyte` is defined as `char` and not `signed char` on OSX? */
-#ifndef __APPLE__
+#ifndef CORRADE_TARGET_APPLE
 static_assert(std::is_same<ALbyte, Byte>::value, "ALbyte is not the same as Byte");
 #else
 static_assert(std::is_signed<ALbyte>::value && sizeof(ALbyte) == 1, "ALbyte does not have the same characteristics as Byte");

CMakeLists.txt
@@ -58,8 +58,12 @@ if(CORRADE_TARGET_NACL)
 elseif(CORRADE_TARGET_ANDROID)
     option(WITH_ANDROIDAPPLICATION "Build AndroidApplication library" OFF)
 
+# OS X-specific application libraries
+elseif(CORRADE_TARGET_APPLE)
+    cmake_dependent_option(WITH_WINDOWLESSCGLAPPLICATION "Build WindowlessCglApplication library" OFF "NOT WITH_MAGNUMINFO;NOT WITH_FONTCONVERTER;NOT WITH_DISTANCEFIELDCONVERTER" ON)
+
 # X11, GLX and EGL-specific application libraries
-elseif(CORRADE_TARGET_UNIX AND NOT APPLE)
+elseif(CORRADE_TARGET_UNIX)
     option(WITH_GLXAPPLICATION "Build GlxApplication library" OFF)
     cmake_dependent_option(WITH_WINDOWLESSGLXAPPLICATION "Build WindowlessGlxApplication library" OFF "NOT WITH_MAGNUMINFO;NOT WITH_FONTCONVERTER;NOT WITH_DISTANCEFIELDCONVERTER" ON)
     cmake_dependent_option(WITH_XEGLAPPLICATION "Build XEglApplication library" OFF "TARGET_GLES" OFF)
@@ -67,10 +71,6 @@ elseif(CORRADE_TARGET_UNIX AND NOT APPLE)
 # Windows-specific application libraries
 elseif(CORRADE_TARGET_WINDOWS)
     cmake_dependent_option(WITH_WINDOWLESSWGLAPPLICATION "Build WindowlessWglApplication library" OFF "NOT WITH_MAGNUMINFO;NOT WITH_FONTCONVERTER;NOT WITH_DISTANCEFIELDCONVERTER" ON)
-
-# OS X-specific application libraries
-elseif(APPLE)
-    cmake_dependent_option(WITH_WINDOWLESSCGLAPPLICATION "Build WindowlessCglApplication library" OFF "NOT WITH_MAGNUMINFO;NOT WITH_FONTCONVERTER;NOT WITH_DISTANCEFIELDCONVERTER" ON)
 endif()
 
 # Platform-independent (almost) application libraries

modules/FindMagnum.cmake
@@ -230,7 +230,7 @@ endif()
 # ${MAGNUM_LIBRARIES} listed in dependencies also after all other library names
 # to avoid linker errors. Applicaiton libraries are often last thus it is
 # +- sufficient to add it there only.
-if(WIN32 OR MAGNUM_BUILD_STATIC)
+if(CORRADE_TARGET_WINDOWS OR MAGNUM_BUILD_STATIC)
     set(_WINDOWCONTEXT_MAGNUM_LIBRARIES_DEPENDENCY ${MAGNUM_LIBRARIES})
 endif()
 

src/Magnum/CMakeLists.txt
@@ -242,7 +242,7 @@ if(BUILD_TESTS)
 
     # On Windows we need to install first and then run the tests to avoid "DLL
     # not found" hell, thus we need to install this too
-    if(WIN32 AND NOT CMAKE_CROSSCOMPILING)
+    if(CORRADE_TARGET_WINDOWS AND NOT CMAKE_CROSSCOMPILING)
         install(TARGETS MagnumMathTestLib
             RUNTIME DESTINATION ${MAGNUM_BINARY_INSTALL_DIR}
             LIBRARY DESTINATION ${MAGNUM_LIBRARY_INSTALL_DIR}

src/Magnum/MeshTools/CMakeLists.txt
@@ -91,7 +91,7 @@ if(BUILD_TESTS)
 
     # On Windows we need to install first and then run the tests to avoid "DLL
     # not found" hell, thus we need to install this too
-    if(WIN32 AND NOT CMAKE_CROSSCOMPILING)
+    if(CORRADE_TARGET_WINDOWS AND NOT CMAKE_CROSSCOMPILING)
         install(TARGETS MagnumMeshToolsTestLib
             RUNTIME DESTINATION ${MAGNUM_BINARY_INSTALL_DIR}
             LIBRARY DESTINATION ${MAGNUM_LIBRARY_INSTALL_DIR}

src/Magnum/Platform/CMakeLists.txt
@@ -232,14 +232,14 @@ endif()
 # Magnum Info
 if(WITH_MAGNUMINFO)
     add_executable(magnum-info magnum-info.cpp)
-    if(UNIX AND NOT CORRADE_TARGET_NACL AND NOT APPLE)
-        target_link_libraries(magnum-info MagnumWindowlessGlxApplication ${X11_LIBRARIES})
+    if(CORRADE_TARGET_APPLE)
+        target_link_libraries(magnum-info MagnumWindowlessCglApplication)
     elseif(CORRADE_TARGET_NACL)
         target_link_libraries(magnum-info MagnumWindowlessNaClApplication ppapi_cpp ppapi)
+    elseif(CORRADE_TARGET_UNIX)
+        target_link_libraries(magnum-info MagnumWindowlessGlxApplication ${X11_LIBRARIES})
     elseif(CORRADE_TARGET_WINDOWS)
         target_link_libraries(magnum-info MagnumWindowlessWglApplication)
-    elseif(APPLE)
-        target_link_libraries(magnum-info MagnumWindowlessCglApplication)
     else()
         message(FATAL_ERROR "magnum-info is not available on this platform. Set WITH_MAGNUMINFO to OFF to skip building it.")
     endif()

src/Magnum/Platform/Sdl2Application.cpp
@@ -143,7 +143,7 @@ bool Sdl2Application::tryCreateContext(const Configuration& configuration) {
        in Apple's GL drivers, thus we would be forever stuck on 2.1 without the
        new features. In practice SDL fails to create 2.1 context on recent OS X
        versions. */
-    #elif defined(__APPLE__)
+    #elif defined(CORRADE_TARGET_APPLE)
     else {
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
@@ -161,7 +161,7 @@ bool Sdl2Application::tryCreateContext(const Configuration& configuration) {
     }
 
     /* Fall back to GL 2.1, if 3.2 context creation fails on OS X */
-    #ifdef __APPLE__
+    #ifdef CORRADE_TARGET_APPLE
     if(!(context = SDL_GL_CreateContext(window))){
         Warning() << "Platform::Sdl2Application::tryCreateContext(): cannot create core context:" << SDL_GetError() << "(falling back to compatibility context)";
         SDL_DestroyWindow(window);

src/Magnum/Platform/magnum-info.cpp
@@ -52,7 +52,7 @@
 
 #ifdef CORRADE_TARGET_NACL
 #include "Magnum/Platform/WindowlessNaClApplication.h"
-#elif defined(__APPLE__)
+#elif defined(CORRADE_TARGET_APPLE)
 #include "Magnum/Platform/WindowlessCglApplication.h"
 #elif defined(CORRADE_TARGET_UNIX)
 #include "Magnum/Platform/WindowlessGlxApplication.h"
@@ -102,10 +102,14 @@ MagnumInfo::MagnumInfo(const Arguments& arguments): Platform::WindowlessApplicat
 
     #ifdef CORRADE_TARGET_NACL
     Debug() << "Used application: Platform::WindowlessNaClApplication";
-    #elif defined(__APPLE__)
+    #elif defined(CORRADE_TARGET_APPLE)
     Debug() << "Used application: Platform::WindowlessCglApplication";
-    #else
+    #elif defined(CORRADE_TARGET_UNIX)
     Debug() << "Used application: Platform::WindowlessGlxApplication";
+    #elif defined(CORRADE_TARGET_WINDOWS)
+    Debug() << "Used application: Platform::WindowlessWglApplication";
+    #else
+    #error No windowless application available on this platform
     #endif
     Debug() << "Compilation flags:";
     #ifdef CORRADE_GCC47_COMPATIBILITY

commit fb515177090b191062bd8a88236a3b065a67f35a

Audio: fix compilation on OSX.
Broken since c7b23a884fabea1298ac4e68d7fa4e8da6ab31a6 (missing include).
Thanks to @chris-chambers for the hint.

src/Magnum/Audio/Audio.cpp
@@ -26,6 +26,7 @@
 #include <type_traits>
 #include <al.h>
 
+#include "Corrade/configure.h"
 #include "Magnum/Types.h"
 
 namespace Magnum { namespace Audio {