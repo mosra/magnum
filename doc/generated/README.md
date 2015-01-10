7ebec50d2b99a88c90650faa1c5e5091aae0f312
Vladimír Vondruš <mosra@centrum.cz>
2015-01-10 18:23:15 +0100

    doc: updated also Getting Started image to the new color.

--- doc/generated/README.md
+++ doc/generated/README.md
@@ -1,9 +1,9 @@
 Source files for images in Magnum documentation
 -----------------------------------------------
 
-Compile and install Magnum with windowless application for your platform and
-`magnum-distancefieldconverter` utility and any `PngImporter` and
-`PngImageConverter` plugins from Magnum Plugins.
+Compile and install Magnum with `Sdl2Application`, windowless application for
+your platform and `magnum-distancefieldconverter` utility and any `PngImporter`
+and `PngImageConverter` plugins from Magnum Plugins.
 
 Create build dir, point CMake to this directory and compile the executables:
 
@@ -11,6 +11,21 @@ Create build dir, point CMake to this directory and compile the executables:
     cd build-doc
     cmake ../doc/generated
     cmake --build .
+
+### "Getting started" image
+
+Displayed by the `hello` executable. Run the app and take screenshot using
+KSnapshot (including decorations, 880x707). Similarly for the gray version. The
+resulting files should be resized to half the size and without alpha channel
+using imagemagick:
+
+```bash
+mogrify -flatten -background '#ffffff' -resize 440 getting-started.png
+mogrify -flatten -background '#ffffff' -resize 440 getting-started-blue.png
+```
+
+The output printed by the application can be used to update the example output
+in `doc/getting-started.dox`.
 
 ### Shader images
 
---
+++ doc/generated/hello.cpp
@@ -0,0 +1,54 @@
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013, 2014
+              Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include <Magnum/Color.h>
+#include <Magnum/DefaultFramebuffer.h>
+#include <Magnum/Context.h>
+#include <Magnum/Renderer.h>
+#include <Magnum/Version.h>
+#include <Magnum/Platform/Sdl2Application.h>
+
+using namespace Magnum;
+
+class Hello: public Platform::Application {
+public:
+    explicit Hello(const Arguments& arguments);
+
+private:
+    void drawEvent() override;
+};
+
+Hello::Hello(const Arguments& arguments): Platform::Application(arguments) {
+    Renderer::setClearColor(Color3::fromHSV(216.0_degf, 0.85f, 1.0f));
+    Debug() << "Hello! This application is running on" << Context::current()->version()
+            << "using" << Context::current()->rendererString();
+}
+
+void Hello::drawEvent() {
+    defaultFramebuffer.clear(FramebufferClear::Color);
+    swapBuffers();
+}
+
+MAGNUM_APPLICATION_MAIN(Hello)
