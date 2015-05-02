50d62dd38ae5342518098e1f135467718fd1d815
Vladimír Vondruš <mosra@centrum.cz>
2015-05-02 17:23:31 +0200

    GCC 4.4 compatibility: explicit std::reference_wrapper constructor.

--- src/Magnum/Test/AbstractShaderProgramGLTest.cpp
+++ src/Magnum/Test/AbstractShaderProgramGLTest.cpp
@@ -302,8 +302,9 @@ void AbstractShaderProgramGLTest::uniformLocationOptimizedOut() {
     vert.addSource("void main() { gl_Position = vec4(0.0); }");
     frag.addSource("void main() { gl_FragColor = vec4(1.0); }");
 
-    CORRADE_VERIFY(Shader::compile({vert, frag}));
-    program.attachShaders({vert, frag});
+    /* GCC 4.4 has explicit std::reference_wrapper constructor */
+    CORRADE_VERIFY(Shader::compile({std::ref(vert), std::ref(frag)}));
+    program.attachShaders({std::ref(vert), std::ref(frag)});
     CORRADE_VERIFY(program.link());
 
     std::ostringstream out;
