b2d7f4ecc7d15882e0c1c260c20c0a811c66cd1e
Vladimír Vondruš <mosra@centrum.cz>
2015-05-08 12:11:52 +0200

    Revert "Math: use Vector::max() instead of custom ugly solution."
    This reverts commit 71db38cb2ff4b8e2fcd1d3877aed580cf7a4e846. The test
    passes again.

--- src/Magnum/Math/Quaternion.h
+++ src/Magnum/Math/Quaternion.h
@@ -543,7 +543,10 @@ template<class T> Quaternion<T> quaternionFromMatrix(const Matrix3x3<T>& m) {
     }
 
     /* Diagonal is negative */
-    const std::size_t i = diagonal.max();
+    std::size_t i = 0;
+    if(diagonal[1] > diagonal[0]) i = 1;
+    if(diagonal[2] > diagonal[i]) i = 2;
+
     const std::size_t j = (i + 1) % 3;
     const std::size_t k = (i + 2) % 3;
 
