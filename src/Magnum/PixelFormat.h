2ec073ad37b07898bc1a9987b4cf019572a34c85
Vladimír Vondruš <mosra@centrum.cz>
2015-09-28 12:58:59 +0200

    Superfluous comma.

--- src/Magnum/PixelFormat.h
+++ src/Magnum/PixelFormat.h
@@ -1188,7 +1188,7 @@ enum class CompressedPixelFormat: GLenum {
      *      for 3D textures and HDR profile
      * @requires_gles ASTC texture compression is not available in WebGL.
      */
-    SRGB8Alpha8Astc12x12 = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_12x12_KHR,
+    SRGB8Alpha8Astc12x12 = GL_COMPRESSED_SRGB8_ALPHA8_ASTC_12x12_KHR
     #endif
 };
 
