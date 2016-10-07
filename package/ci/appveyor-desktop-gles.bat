1ae1f3d41911d04faf9a4960a1fb27e6feeb028d
Vladimír Vondruš <mosra@centrum.cz>
2016-10-07 15:38:00 +0200

    package/ci: add desktop GLES builds on Linux and Windows.

---
+++ package/ci/appveyor-desktop-gles.bat
@@ -0,0 +1,47 @@
+call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" x64 || exit /b
+set PATH=%APPVEYOR_BUILD_FOLDER%/openal/bin/Win64;%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
+
+rem Build Corrade
+git clone --depth 1 git://github.com/mosra/corrade.git || exit /b
+cd corrade || exit /b
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DWITH_INTERCONNECT=OFF ^
+    -G Ninja || exit /b
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
+cd .. && cd ..
+
+rem Build
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DCMAKE_PREFIX_PATH="%APPVEYOR_BUILD_FOLDER%/openal" ^
+    -DTARGET_GLES=ON ^
+    -DTARGET_GLES2=%TARGET_GLES2% ^
+    -DTARGET_DESKTOP_GLES=ON ^
+    -DWITH_AUDIO=ON ^
+    -DWITH_SDL2APPLICATION=OFF ^
+    -DWITH_WINDOWLESSWGLAPPLICATION=ON ^
+    -DWITH_WGLCONTEXT=ON ^
+    -DWITH_MAGNUMFONT=ON ^
+    -DWITH_MAGNUMFONTCONVERTER=ON ^
+    -DWITH_OBJIMPORTER=ON ^
+    -DWITH_TGAIMAGECONVERTER=ON ^
+    -DWITH_TGAIMPORTER=ON ^
+    -DWITH_WAVAUDIOIMPORTER=ON ^
+    -DWITH_DISTANCEFIELDCONVERTER=OFF ^
+    -DWITH_FONTCONVERTER=OFF ^
+    -DWITH_MAGNUMINFO=ON ^
+    -DWITH_AL_INFO=ON ^
+    -DBUILD_TESTS=ON ^
+    -DBUILD_GL_TESTS=ON ^
+    -G Ninja || exit /b
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
+
+rem Test
+ctest -V -E GLTest || exit /b
