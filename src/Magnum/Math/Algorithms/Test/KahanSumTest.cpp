04c7ffdf1b3c7d148526a2791201949468d86b54
sigman <sigman78@gmail.com>
2017-06-14 12:53:40 +0200

    Math/Algorithms: MSVC 2017 needs operator==() as well.

--- src/Magnum/Math/Algorithms/Test/KahanSumTest.cpp
+++ src/Magnum/Math/Algorithms/Test/KahanSumTest.cpp
@@ -68,6 +68,8 @@ template<class T> struct Iterator
 {
     explicit Iterator(T value, std::size_t i = 0): _value{value}, _i{i} {}
 
+    /* MSVC 2017 needs operator==() as well */
+    bool operator==(const Iterator& other) const { return _i == other._i; }
     bool operator!=(const Iterator& other) const { return _i != other._i; }
 
     Iterator& operator++() {
