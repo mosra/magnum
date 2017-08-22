c77782c3f43a6f635deabaf9e9f4fbae9201ff98
Vladimír Vondruš <mosra@centrum.cz>
2017-08-22 22:41:39 +0200

    Audio: ported magnum-al-info to Emscripten.

--- package/ci/travis-emscripten.sh
+++ package/ci/travis-emscripten.sh
@@ -56,6 +56,7 @@ cmake .. \
     -DWITH_TGAIMPORTER=ON \
     -DWITH_WAVAUDIOIMPORTER=ON \
     -DWITH_MAGNUMINFO=ON \
+    -DWITH_AL_INFO=ON \
     -DBUILD_TESTS=ON \
     -DTARGET_GLES2=$TARGET_GLES2
 # Otherwise the job gets killed (probably because using too much memory)
