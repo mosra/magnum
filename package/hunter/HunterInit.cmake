a98873ae81359907c3c64c691e924a49bfc33c17
Pascal Thomet <pthomet@gmail.com>
2018-11-30 09:38:31 +0100

    package/hunter/HunterInit.cmake : add fixme warning

--- package/hunter/HunterInit.cmake
+++ package/hunter/HunterInit.cmake
@@ -4,6 +4,7 @@ file(
   ${CMAKE_CURRENT_LIST_DIR}//HunterGate.cmake
 )
 
+# FIXME : will need to replace this url with the main hunter url !
 include("${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake")
 HunterGate(
   URL "https://github.com/pthom/hunter/archive/test-corrade-v2018.10.tar.gz"
