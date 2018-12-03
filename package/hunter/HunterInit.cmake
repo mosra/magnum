683a81a407d89dff5c6f6a7fd8f557561da97940
Pascal Thomet <pthomet@gmail.com>
2018-12-03 13:29:34 +0100

    package/hunter/HunterInit.cmake : wrong url

--- package/hunter/HunterInit.cmake
+++ package/hunter/HunterInit.cmake
@@ -7,6 +7,6 @@ file(
 # FIXME : will need to replace this url with the main hunter url !
 include("${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake")
 HunterGate(
-  URL "https://github.com/pthom/magnum/archive/hunter_rc3.tar.gz"
-  SHA1 "990c49b8bb085c395eebe8a46686552767cffb3f"
+  URL "https://github.com/pthom/hunter/archive/hunter_magnum_rc4.tar.gz"
+  SHA1 "31554df9e988f27f4daa9f4df9529d75007500bf"
 )
