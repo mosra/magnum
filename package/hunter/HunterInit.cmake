2f9046e666ed5c2d3ab4a4c6a6df56d9f1c7e7e3
Pascal Thomet <pthomet@gmail.com>
2018-12-03 11:56:58 +0100

    package/hunter/HunterInit.cmake : use hunter_rc3 release

--- package/hunter/HunterInit.cmake
+++ package/hunter/HunterInit.cmake
@@ -7,6 +7,6 @@ file(
 # FIXME : will need to replace this url with the main hunter url !
 include("${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake")
 HunterGate(
-  URL "https://github.com/pthom/hunter/archive/test-corrade-v2018.10.tar.gz"
-  SHA1 "3415f425f0ff1aa1754b8a2b57c77ba20f5b7bbc"
+  URL "https://github.com/pthom/magnum/archive/hunter_rc3.tar.gz"
+  SHA1 "990c49b8bb085c395eebe8a46686552767cffb3f"
 )
