526254912e8bf862d4700fc47bd475e17d284ca5
Vladimír Vondruš <mosra@centrum.cz>
2020-02-28 13:55:37 +0100

    doc: updated credits and changelog.

--- src/Magnum/Math/Frustum.h
+++ src/Magnum/Math/Frustum.h
@@ -5,7 +5,7 @@
 
     Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019
               Vladimír Vondruš <mosra@centrum.cz>
-    Copyright © 2016 Jonathan Hale <squareys@googlemail.com>
+    Copyright © 2016, 2020 Jonathan Hale <squareys@googlemail.com>
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
