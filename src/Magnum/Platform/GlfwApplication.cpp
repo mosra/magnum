8479be7758642f1df3d90c46d942d6d98d288be1
Tracy Ma <1991md@gmail.com>
2020-09-21 16:36:15 +0800

    Fix: unable to create gl context on desktop with ANGLE + GLFW3

--- src/Magnum/Platform/GlfwApplication.cpp
+++ src/Magnum/Platform/GlfwApplication.cpp
@@ -475,6 +475,7 @@ bool GlfwApplication::tryCreate(const Configuration& configuration, const GLConf
         #endif
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
         glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+        glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
         #endif
     }
 
