de8613e8b1733028efdcbae238d525ba6c327128
Vladimír Vondruš <mosra@centrum.cz>
2020-10-16 19:29:32 +0200

    package/homebrew: update to what's actually recent in the package repo.

--- package/homebrew/magnum.rb
+++ package/homebrew/magnum.rb
@@ -1,9 +1,9 @@
 class Magnum < Formula
   desc "C++11/C++14 graphics middleware for games and data visualization"
   homepage "https://magnum.graphics"
-  url "https://github.com/mosra/magnum/archive/v2019.10.tar.gz"
-  # wget https://github.com/mosra/magnum/archive/v2019.10.tar.gz -O - | sha256sum
-  sha256 "375e8e03b1848b166387baee4ac4082c75a97fd234f01cd9e5187e0161968584"
+  url "https://github.com/mosra/magnum/archive/v2020.06.tar.gz"
+  # wget https://github.com/mosra/magnum/archive/v2020.06.tar.gz -O - | sha256sum
+  sha256 "98dfe802e56614e4e6bf750d9b693de46a5ed0c6eb479b0268f1a20bf34268bf"
   head "git://github.com/mosra/magnum.git"
 
   depends_on "cmake"
@@ -14,7 +14,34 @@ class Magnum < Formula
   def install
     system "mkdir build"
     cd "build" do
-      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DMAGNUM_PLUGINS_DIR=#{HOMEBREW_PREFIX}/lib/magnum", "-DWITH_AUDIO=ON", "-DWITH_GLFWAPPLICATION=ON", "-DWITH_SDL2APPLICATION=ON", "-DWITH_WINDOWLESSCGLAPPLICATION=ON", "-DWITH_CGLCONTEXT=ON", "-DWITH_OPENGLTESTER=ON", "-DWITH_ANYAUDIOIMPORTER=ON", "-DWITH_ANYIMAGECONVERTER=ON", "-DWITH_ANYIMAGEIMPORTER=ON", "-DWITH_ANYSCENECONVERTER=ON", "-DWITH_ANYSCENEIMPORTER=ON", "-DWITH_MAGNUMFONT=ON", "-DWITH_MAGNUMFONTCONVERTER=ON", "-DWITH_OBJIMPORTER=ON", "-DWITH_TGAIMAGECONVERTER=ON", "-DWITH_TGAIMPORTER=ON", "-DWITH_WAVAUDIOIMPORTER=ON", "-DWITH_DISTANCEFIELDCONVERTER=ON", "-DWITH_FONTCONVERTER=ON", "-DWITH_IMAGECONVERTER=ON", "-DWITH_SCENECONVERTER=ON", "-DWITH_GL_INFO=ON", "-DWITH_AL_INFO=ON", ".."
+      system "cmake",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+        "-DMAGNUM_PLUGINS_DIR=#{HOMEBREW_PREFIX}/lib/magnum",
+        "-DWITH_AUDIO=ON",
+        "-DWITH_GLFWAPPLICATION=ON",
+        "-DWITH_SDL2APPLICATION=ON",
+        "-DWITH_WINDOWLESSCGLAPPLICATION=ON",
+        "-DWITH_CGLCONTEXT=ON",
+        "-DWITH_OPENGLTESTER=ON",
+        "-DWITH_ANYAUDIOIMPORTER=ON",
+        "-DWITH_ANYIMAGECONVERTER=ON",
+        "-DWITH_ANYIMAGEIMPORTER=ON",
+        "-DWITH_ANYSCENECONVERTER=ON",
+        "-DWITH_ANYSCENEIMPORTER=ON",
+        "-DWITH_MAGNUMFONT=ON",
+        "-DWITH_MAGNUMFONTCONVERTER=ON",
+        "-DWITH_OBJIMPORTER=ON",
+        "-DWITH_TGAIMAGECONVERTER=ON",
+        "-DWITH_TGAIMPORTER=ON",
+        "-DWITH_WAVAUDIOIMPORTER=ON",
+        "-DWITH_DISTANCEFIELDCONVERTER=ON",
+        "-DWITH_FONTCONVERTER=ON",
+        "-DWITH_IMAGECONVERTER=ON",
+        "-DWITH_SCENECONVERTER=ON",
+        "-DWITH_GL_INFO=ON",
+        "-DWITH_AL_INFO=ON",
+        ".."
       system "cmake", "--build", "."
       system "cmake", "--build", ".", "--target", "install"
     end
