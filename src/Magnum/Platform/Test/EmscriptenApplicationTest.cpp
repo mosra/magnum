110e45d0ea75e5c53677b1c99581cba216e33719
Pablo Escobar <mail@rvrs.in>
2020-11-05 22:26:03 +0100

    Platform: add test for multiple applications on one page.

--- src/Magnum/Platform/Test/EmscriptenApplicationTest.cpp
+++ src/Magnum/Platform/Test/EmscriptenApplicationTest.cpp
@@ -31,6 +31,7 @@
 #include "Magnum/GL/Renderer.h"
 #include "Magnum/GL/DefaultFramebuffer.h"
 #include "Magnum/GL/Mesh.h"
+#include "Magnum/Math/Color.h"
 
 namespace Magnum { namespace Platform { namespace Test {
 
@@ -40,6 +41,9 @@ struct EmscriptenApplicationTest: Platform::Application {
 
     virtual void drawEvent() override {
         Debug() << "draw event";
+        #ifdef CUSTOM_CLEAR_COLOR
+        GL::Renderer::setClearColor({ CUSTOM_CLEAR_COLOR });
+        #endif
         GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
 
         swapBuffers();
