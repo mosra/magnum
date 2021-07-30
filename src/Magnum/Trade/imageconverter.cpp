696e9fb1058a6ae88bccbc3db1c35fecce606a23
Vladimír Vondruš <mosra@centrum.cz>
2021-07-30 12:27:54 +0200

    imageconverter: note how many dimensions the image has in --info.

--- src/Magnum/Trade/imageconverter.cpp
+++ src/Magnum/Trade/imageconverter.cpp
@@ -270,7 +270,10 @@ key=true; configuration subgroups are delimited with /.)")
             for(const Trade::Implementation::ImageInfo& info: infos) {
                 Debug d;
                 if(info.level == 0) {
-                    d << "Image" << info.image << Debug::nospace << ":";
+                    if(info.size.z()) d << "3D image";
+                    else if(info.size.y()) d << "2D image";
+                    else d << "1D image";
+                    d << info.image << Debug::nospace << ":";
                     if(!info.name.empty()) d << info.name;
                     if(!compact) d << Debug::newline;
                 }
