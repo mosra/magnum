754e01079448609bbe94cb23bbba735dfc8120e2
Vladimír Vondruš <mosra@centrum.cz>
2021-11-02 17:56:14 +0100

    package/homebrew: make this work on ARM Macs as well.
    Thank you, Homebrew! FFS!

--- package/homebrew/magnum.rb
+++ package/homebrew/magnum.rb
@@ -16,6 +16,11 @@ class Magnum < Formula
     cd "build" do
       system "cmake",
         *std_cmake_args,
+        # Without this, ARM builds will try to look for dependencies in
+        # /usr/local/lib and /usr/lib (which are the default locations) instead
+        # of /opt/homebrew/lib which is dedicated for ARM binaries. Please
+        # complain to Homebrew about this insane non-obvious filesystem layout.
+        "-DCMAKE_INSTALL_NAME_DIR:STRING=#{lib}",
         # Without this, it will try to look for plugins somewhere deep in
         # homebrew's Cellar in per-package directories instead of in the
         # location everything's symlinked to, thus finding only magnum's core
