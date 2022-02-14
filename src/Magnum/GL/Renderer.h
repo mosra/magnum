822fa0d6446c51b4f20b7d877863b1f89ca976ce
Vladimír Vondruš <mosra@centrum.cz>
2022-02-14 19:37:19 +0100

    Updated copyright year.

--- src/Magnum/GL/Renderer.h
+++ src/Magnum/GL/Renderer.h
@@ -4,7 +4,7 @@
     This file is part of Magnum.
 
     Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
-                2020, 2021 Vladimír Vondruš <mosra@centrum.cz>
+                2020, 2021, 2022 Vladimír Vondruš <mosra@centrum.cz>
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
