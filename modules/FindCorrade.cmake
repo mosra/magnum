63cb469b292fa7427c6cee17cb2e352721248792
Vladimír Vondruš <mosra@centrum.cz>
2022-02-14 19:19:11 +0100

    modules: updated FindCorrade and FindNodeJs.

--- modules/FindCorrade.cmake
+++ modules/FindCorrade.cmake
@@ -264,7 +264,7 @@
 #   This file is part of Corrade.
 #
 #   Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
-#               2017, 2018, 2019, 2020, 2021
+#               2017, 2018, 2019, 2020, 2021, 2022
 #             Vladimír Vondruš <mosra@centrum.cz>
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a
