c29c5d67f74fdc4aca84f04d9be42869b9333ba3
Vladimír Vondruš <mosra@centrum.cz>
2022-08-20 19:00:16 +0200

    TgaImageConverter: fix a non-deprecated build.
    I should really clean up the compatibility includes, this is getting
    unbearable. Can't even push straight to master without thinking.

--- src/MagnumPlugins/TgaImageConverter/Test/TgaImageConverterTest.cpp
+++ src/MagnumPlugins/TgaImageConverter/Test/TgaImageConverterTest.cpp
@@ -27,7 +27,7 @@
 #include <tuple>
 #include <Corrade/Containers/Array.h>
 #include <Corrade/Containers/Optional.h>
-#include <Corrade/Containers/StringView.h>
+#include <Corrade/Containers/String.h>
 #include <Corrade/TestSuite/Tester.h>
 #include <Corrade/TestSuite/Compare/Container.h>
 #include <Corrade/Utility/DebugStl.h> /** @todo remove once Debug is stream-free */
