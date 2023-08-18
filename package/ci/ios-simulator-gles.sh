6cb0b226591b87b1b86c2d2741409055f37ef27b
Vladimír Vondruš <mosra@centrum.cz>
2023-08-18 13:04:11 +0200

    package/ci: can't test on Xcode 11 anymore.
    Also disable iOS tests until https://github.com/mosra/corrade/pull/176
    is resolved.

--- package/ci/ios-simulator-gles.sh
+++ package/ci/ios-simulator-gles.sh
@@ -41,7 +41,7 @@ cd ../..
 curl --insecure -O https://www.libsdl.org/release/SDL2-2.0.10.tar.gz
 tar -xzvf SDL2-2.0.10.tar.gz
 cd SDL2-2.0.10/Xcode-iOS/SDL
-set -o pipefail && xcodebuild -sdk iphonesimulator13.7 -jobs $XCODE_JOBS -parallelizeTargets | xcbeautify
+set -o pipefail && xcodebuild -sdk iphonesimulator -jobs $XCODE_JOBS -parallelizeTargets | xcbeautify
 cp build/Release-iphonesimulator/libSDL2.a $HOME/deps/lib
 mkdir -p $HOME/deps/include/SDL2
 cp -R ../../include/* $HOME/deps/include/SDL2
@@ -79,10 +79,12 @@ cmake .. \
     -DMAGNUM_BUILD_GL_TESTS=ON \
     -G Xcode
 set -o pipefail && cmake --build . --config Release -j$XCODE_JOBS | xcbeautify
+
+# TODO enable again once https://github.com/mosra/corrade/pull/176 is resolved
 # TODO: find a better way to avoid
 # Library not loaded: /System/Library/Frameworks/OpenGLES.framework/OpenGLES
 # error
-DYLD_FALLBACK_LIBRARY_PATH="/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 12.4.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks/OpenGLES.framework" DYLD_FALLBACK_FRAMEWORK_PATH="/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 12.4.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks" CORRADE_TEST_COLOR=ON ctest -V -C Release -E "GLTest|GLBenchmark"
+# DYLD_FALLBACK_LIBRARY_PATH="/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 12.4.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks/OpenGLES.framework" DYLD_FALLBACK_FRAMEWORK_PATH="/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 12.4.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks" CORRADE_TEST_COLOR=ON ctest -V -C Release -E "GLTest|GLBenchmark"
 
 # Test install, after running the tests as for them it shouldn't be needed
 set -o pipefail && cmake --build . --config Release --target install -j$XCODE_JOBS | xcbeautify
