mosra/magnum

commit 87c7eea1e256c7ef1c53f1d0e2ec8b3e34bc3ae1

Deprecate MAGNUM_TARGET_GLES3.
Originally (2012? 2013?) I expected that there would eventually be
OpenGL ES 4.0, thus it made sense to differentiate between ES2, ES3 and
something else ES yet unknown. But as ES4 was increasingly unlikely to
happen, the internal code treated MAGNUM_TARGET_GLES3 as a simple
inverse of MAGNUM_TARGET_GLES2, and only in a very few places,
only adding confusion.

Thus it's now deprecated and defined as a simple inverse of
MAGNUM_TARGET_GLES2 on MAGNUM_TARGET_GLES builds, and none of the
internal code uses it anymore.

src/Magnum/Platform/EmscriptenApplication.cpp
@@ -373,12 +373,10 @@ bool EmscriptenApplication::tryCreate(const Configuration& configuration, const 
     attrs.enableExtensionsByDefault =
         !!(glConfiguration.flags() & GLConfiguration::Flag::EnableExtensionsByDefault);
 
-    #ifdef MAGNUM_TARGET_GLES3 /* WebGL 2 */
-    attrs.majorVersion = 2;
-    #elif defined(MAGNUM_TARGET_GLES2) /* WebGL 1 */
-    attrs.minorVersion = 1;
-    #else
-    #error unsupported OpenGL ES version
+    #ifdef MAGNUM_TARGET_GLES2 /* WebGL 1 */
+    attrs.majorVersion = 1;
+    #else                      /* WebGL 2 */
+    attrs.minorVersion = 2;
     #endif
 
     std::ostream* verbose = _verboseLog ? Debug::output() : nullptr;

CMakeLists.txt
@@ -594,7 +594,7 @@ endif()
 
 # Dependent configuration variables
 if(MAGNUM_TARGET_GLES)
-    if(NOT MAGNUM_TARGET_GLES2)
+    if(MAGNUM_BUILD_DEPRECATED AND NOT MAGNUM_TARGET_GLES2)
         set(MAGNUM_TARGET_GLES3 1)
     endif()
     if(CORRADE_TARGET_EMSCRIPTEN)

doc/cmake.dox
@@ -326,9 +326,9 @@ are also available as preprocessor variables if including
 -   `MAGNUM_TARGET_GL` --- Defined if compiled with OpenGL interoperability
     enabled
 -   `MAGNUM_TARGET_GLES` --- Defined if compiled for OpenGL ES
--   `MAGNUM_TARGET_GLES2` --- Defined if compiled for OpenGL ES 2.0
--   `MAGNUM_TARGET_GLES3` --- Defined if compiled for OpenGL ES 3.0
 -   `MAGNUM_TARGET_WEBGL` --- Defined if compiled for WebGL
+-   `MAGNUM_TARGET_GLES2` --- Defined if compiled for OpenGL ES 2.0 / WebGL 1
+    instead of OpenGL ES 3.0+ / WebGL 2
 -   `MAGNUM_TARGET_EGL` --- Defined if compiled for EGL instead of a
     platform-specific OpenGL support library such as CGL, EAGL, GLX or WGL.
 -   `MAGNUM_TARGET_VK` --- Defined if compiled with Vulkan interoperability
@@ -344,6 +344,8 @@ release:
     Android, Emscripten or Windows RT. Use @ref MAGNUM_TARGET_EGL instead.
 -   `MAGNUM_TARGET_DESKTOP_GLES` --- Defined if compiled for OpenGL ES but
     GLX / WGL is used instead of EGL. Use @ref MAGNUM_TARGET_EGL instead.
+-   `MAGNUM_TARGET_GLES3` --- Defined if compiled for OpenGL ES 3.0+ / WebGL 2.
+    Use an inverse of the @ref MAGNUM_TARGET_GLES2 variable instead.
 
 Corrade library provides also its own set of CMake macros and variables, see
 @ref corrade-cmake "its documentation" for more information.

doc/opengl.dox
@@ -146,10 +146,8 @@ The following symbols are not defined when targeting OpenGL ES or WebGL.
 */
 
 /** @page requires-gles30 Functionality requiring OpenGL ES 3.0
-@m_footernavigation
-
-@see @ref MAGNUM_TARGET_GLES3
-*/
+ * @m_footernavigation
+ */
 
 /** @page requires-gles31 Functionality requiring OpenGL ES 3.1
 @m_footernavigation

doc/portability.dox
@@ -42,9 +42,9 @@ format is not supported.
 If you include @ref Magnum.h, you get these predefined macros:
 
 -   @ref MAGNUM_TARGET_GLES if targeting OpenGL ES
--   @ref MAGNUM_TARGET_GLES2 if targeting OpenGL ES 2.0
--   @ref MAGNUM_TARGET_GLES3 if targeting OpenGL ES 3.0
 -   @ref MAGNUM_TARGET_WEBGL if targeting WebGL
+-   @ref MAGNUM_TARGET_GLES2 if targeting OpenGL ES 2.0 / WebGL 1 instead of
+    OpenGL ES 3.0+ / WebGL 2
 
 Example usage:
 

modules/FindMagnum.cmake
@@ -135,9 +135,9 @@
 #   globals unique even across different shared libraries
 #  MAGNUM_TARGET_GL             - Defined if compiled with OpenGL interop
 #  MAGNUM_TARGET_GLES           - Defined if compiled for OpenGL ES
-#  MAGNUM_TARGET_GLES2          - Defined if compiled for OpenGL ES 2.0
-#  MAGNUM_TARGET_GLES3          - Defined if compiled for OpenGL ES 3.0
 #  MAGNUM_TARGET_WEBGL          - Defined if compiled for WebGL
+#  MAGNUM_TARGET_GLES2          - Defined if compiled for OpenGL ES 2.0 / WebGL
+#   1 instead of OpenGL ES 3.0+ / WebGL 2
 #  MAGNUM_TARGET_EGL            - Defined if compiled for EGL instead of a
 #   platform-specific OpenGL support library like CGL, EAGL, GLX or WGL
 #  MAGNUM_TARGET_VK             - Defined if compiled with Vulkan interop
@@ -152,6 +152,8 @@
 #   Android, Emscripten or Windows RT. Use MAGNUM_TARGET_EGL instead.
 #  MAGNUM_TARGET_DESKTOP_GLES`  - Defined if compiled for OpenGL ES but
 #   GLX / WGL is used instead of EGL. Use MAGNUM_TARGET_EGL instead.
+#  MAGNUM_TARGET_GLES3          - Defined if compiled for OpenGL ES 3.0+ /
+#   WebGL 2. Use an inverse of the MAGNUM_TARGET_GLES2 variable instead.
 #
 # Additionally these variables are defined for internal usage:
 #
@@ -917,7 +919,7 @@ foreach(_component ${Magnum_FIND_COMPONENTS})
                 find_package(OpenGLES2 REQUIRED)
                 set_property(TARGET Magnum::${_component} APPEND PROPERTY
                     INTERFACE_LINK_LIBRARIES OpenGLES2::OpenGLES2)
-            elseif(MAGNUM_TARGET_GLES3)
+            else()
                 find_package(OpenGLES3 REQUIRED)
                 set_property(TARGET Magnum::${_component} APPEND PROPERTY
                     INTERFACE_LINK_LIBRARIES OpenGLES3::OpenGLES3)

src/Magnum/GL/Context.cpp
@@ -1137,7 +1137,7 @@ Containers::Array<Containers::StringView> Context::extensionStrings() const {
        on GL 2.1. Happens with Mesa's zink that's just 2.1 currently (Apr 2020)
        even though for other backends Mesa exposes this. */
     #ifndef MAGNUM_TARGET_GLES2
-    #ifndef MAGNUM_TARGET_GLES3
+    #ifndef MAGNUM_TARGET_GLES
     if(isVersionSupported(Version::GL300))
     #endif
     {
@@ -1148,14 +1148,15 @@ Containers::Array<Containers::StringView> Context::extensionStrings() const {
             extensions[i] = {reinterpret_cast<const char*>(glGetStringi(GL_EXTENSIONS, i)), Containers::StringViewFlag::Global};
         return extensions;
     }
-    #ifndef MAGNUM_TARGET_GLES3
+    #ifndef MAGNUM_TARGET_GLES
     else
     #endif
     #endif
-
-    #ifndef MAGNUM_TARGET_GLES3
-    /* OpenGL 2.1 / OpenGL ES 2.0 doesn't have glGetStringi() */
-    return Containers::StringView{reinterpret_cast<const char*>(glGetString(GL_EXTENSIONS)), Containers::StringViewFlag::Global}.splitOnWhitespaceWithoutEmptyParts();
+    #if !defined(MAGNUM_TARGET_GLES) || defined(MAGNUM_TARGET_GLES2)
+    {
+        /* OpenGL 2.1 / OpenGL ES 2.0 doesn't have glGetStringi() */
+        return Containers::StringView{reinterpret_cast<const char*>(glGetString(GL_EXTENSIONS)), Containers::StringViewFlag::Global}.splitOnWhitespaceWithoutEmptyParts();
+    }
     #endif
 }
 

src/Magnum/Magnum.h
@@ -139,7 +139,7 @@ Enabled by default in a static build. On Windows see also the
 Defined if the engine is built with OpenGL interoperability enabled --- extra
 APIs in various libraries interacting with the @ref Magnum::GL "GL" library.
 Enabled by default in case the @ref Magnum::GL "GL" library is built.
-@see @ref MAGNUM_TARGET_GLES2, @ref MAGNUM_TARGET_GLES3,
+@see @ref MAGNUM_TARGET_GLES, @ref MAGNUM_TARGET_GLES2,
     @ref MAGNUM_TARGET_EGL, @ref building, @ref cmake
 */
 #define MAGNUM_TARGET_GL
@@ -149,47 +149,45 @@ Enabled by default in case the @ref Magnum::GL "GL" library is built.
 @brief OpenGL ES target
 
 Defined if the engine is built for OpenGL ES 3.0 or OpenGL ES 2.0.
-@see @ref MAGNUM_TARGET_GLES2, @ref MAGNUM_TARGET_GLES3,
-    @ref MAGNUM_TARGET_EGL, @ref building, @ref cmake
+@see @ref MAGNUM_TARGET_GLES2, @ref MAGNUM_TARGET_EGL, @ref building,
+    @ref cmake
 */
 #define MAGNUM_TARGET_GLES
 #undef MAGNUM_TARGET_GLES
 
 /**
-@brief OpenGL ES 2.0 target
+@brief WebGL target
 
-Defined if the engine is built for OpenGL ES 2.0. Implies also
-@ref MAGNUM_TARGET_GLES.
-@see @ref MAGNUM_TARGET_GLES3, @ref MAGNUM_TARGET_EGL, @ref building,
-    @ref cmake
+Defined if the engine is built for WebGL (using Emscripten). WebGL 1 is nearly
+equivalent to OpenGL ES 2.0 and WebGL 2 to OpenGL ES 3.0, thus in most cases
+you don't need to treat it differently, but there are some specific
+restrictions and differences [compared to OpenGL ES 2.0](http://www.khronos.org/registry/webgl/specs/latest/1.0/#6)
+and to [OpenGL ES 3.0](https://registry.khronos.org/webgl/specs/latest/2.0/#5)
+which you might want to be aware of. Implies also @ref MAGNUM_TARGET_GLES.
+@see @ref MAGNUM_TARGET_GLES2 @ref CORRADE_TARGET_EMSCRIPTEN, @ref building, @ref cmake
+*/
+#define MAGNUM_TARGET_WEBGL
+#undef MAGNUM_TARGET_WEBGL
+
+/**
+@brief OpenGL ES 2.0 / WebGL 1.0 target
+
+Defined if the engine is built for OpenGL ES 2.0 / WebGL 1.0 instead of OpenGL
+ES 3.0+ / WebGL 2. Implies also @ref MAGNUM_TARGET_GLES.
+@see @ref MAGNUM_TARGET_EGL, @ref building, @ref cmake
 */
 #define MAGNUM_TARGET_GLES2
 #undef MAGNUM_TARGET_GLES2
 
+#ifdef MAGNUM_BUILD_DEPRECATED
 /**
-@brief OpenGL ES 3.0 target
-
-Defined if the engine is built for OpenGL ES 3.0. Implies also
-@ref MAGNUM_TARGET_GLES.
-@see @ref MAGNUM_TARGET_GLES2, @ref MAGNUM_TARGET_EGL, @ref building,
-    @ref cmake
+@brief OpenGL ES 3.0+ / WebGL 2.0 target
+@m_deprecated_since_latest This macro is simply an inverse of
+    @ref MAGNUM_TARGET_GLES2, use that one instead.
 */
 #define MAGNUM_TARGET_GLES3
 #undef MAGNUM_TARGET_GLES3
-
-/**
-@brief WebGL target
-
-Defined if the engine is built for WebGL (using Emscripten). WebGL is nearly
-equivalent to OpenGL ES 2.0, thus in most cases you don't need to treat it
-differently, but there are some
-[specific restrictions and features](http://www.khronos.org/registry/webgl/specs/latest/1.0/#6)
-which you might want to be aware of. Implies also @ref MAGNUM_TARGET_GLES and
-@ref MAGNUM_TARGET_GLES2.
-@see @ref CORRADE_TARGET_EMSCRIPTEN, @ref building, @ref cmake
-*/
-#define MAGNUM_TARGET_WEBGL
-#undef MAGNUM_TARGET_WEBGL
+#endif
 
 /**
 @brief EGL target

commit 75c25da6e5c065e785f411bd47feb007458ccae0

Platform: fix EmscriptenApplication context creation on WebGL 2.
Insufficient testing, sorry. This got broken after MAGNUM_TARGET_GLES3
deprecation in 87c7eea1e256c7ef1c53f1d0e2ec8b3e34bc3ae1, but the stupid
variable name was there before already, and it just worked for some
reason, but when the condition got flipped, setting *minor* version to 2
didn't do the right thing anymore. And of course, typical Emscripten,
it didn't complain at all in either case.

Took me an embarrassingly long time to spot what was wrong.

src/Magnum/Platform/EmscriptenApplication.cpp
@@ -376,7 +376,7 @@ bool EmscriptenApplication::tryCreate(const Configuration& configuration, const 
     #ifdef MAGNUM_TARGET_GLES2 /* WebGL 1 */
     attrs.majorVersion = 1;
     #else                      /* WebGL 2 */
-    attrs.minorVersion = 2;
+    attrs.majorVersion = 2;
     #endif
 
     std::ostream* verbose = _verboseLog ? Debug::output() : nullptr;