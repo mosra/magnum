09bfd2547575df47358da92f98db01bb74dd5e38
Ben-Bingham <ben@benbingham.ca>
2024-05-07 15:34:20 -0600

    Made arguments take (and store) in argc by value instead of reference

--- src/Magnum/Platform/Sdl2Application.h
+++ src/Magnum/Platform/Sdl2Application.h
@@ -484,9 +484,9 @@ class Sdl2Application {
         /** @brief Application arguments */
         struct Arguments {
             /** @brief Constructor */
-            /*implicit*/ constexpr Arguments(int& argc, char** argv) noexcept: argc{argc}, argv{argv} {}
+            /*implicit*/ constexpr Arguments(int argc, char** argv) noexcept: argc{argc}, argv{argv} {}
 
-            int& argc;      /**< @brief Argument count */
+            int argc;       /**< @brief Argument count */
             char** argv;    /**< @brief Argument values */
         };
 
