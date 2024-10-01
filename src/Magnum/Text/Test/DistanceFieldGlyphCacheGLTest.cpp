mosra/magnum

commit 6707534ce65375a3b79598f6d08f125071ee02c9

Text: rework AbstractGlyphCache for better flexibility and efficiency.
The class now supports incremental filling, multiple fonts, texture
arrays, removes all reliance on STL containers and is finally properly
documented.

To avoid complete breakage of every use, as much as possible was kept as
deprecated APIs -- in particular the reserve() with the nasty
std::vectors, the insert() that assumes a 2D cache and a single font
and textureSize() that returns a 2D vector. Those behave the same as
before, but will assert if the cache is an array or contains more than
one font.

On the other hand, begin() / end() access with std::unordered_map iterators
(ew!) was removed as the internals simply aren't a hashmap anymore. The
image() that returned an Image2D is now used to fill the glyph cache
instead of querying its potentially processed contents, and returns a
MutableImageView3D. I considered keeping it and adding sourceImage()
instead, but such naming turned out to be too inconsistent. For querying
processed image data (such as with the distance field cache) there's a
new processedImage() query, guarded by new GlyphCacheFeature bits -- if
both ImageProcessing and ProcessedImageDownload is set, it can be used
to retrieve the processed image (so, similar as ImageDownload was
before), and if neither is set, the cache contents are queryable
directly through image(), without needing any special support from
the GPU API.

Existing code is updated only in the minimal way possible to ensure that
no serious breakage was introduced by reimplementing the deprecated APIs
on top of the new backend. Porting away from deprecated APIs will be
done in next commits. The GlyphCache and DistanceFieldGlyphCache have
their public API kept intact for now, as a similar rework will be needed
for them as well.

Additionally, the MagnumFont and MagnumFontConverter plugins aren't
compiling yet as they require substantial changes to deal with the new

src/Magnum/Text/DistanceFieldGlyphCache.cpp
@@ -25,13 +25,16 @@
 
 #include "DistanceFieldGlyphCache.h"
 
+#include "Magnum/Image.h"
 #include "Magnum/ImageView.h"
+#include "Magnum/PixelFormat.h"
 #include "Magnum/GL/Context.h"
 #include "Magnum/GL/Extensions.h"
 #ifndef CORRADE_NO_ASSERT
 #include "Magnum/GL/PixelFormat.h"
 #endif
 #include "Magnum/GL/TextureFormat.h"
+#include "Magnum/Math/Range.h"
 #include "Magnum/TextureTools/DistanceField.h"
 
 namespace Magnum { namespace Text {
@@ -59,40 +62,48 @@ DistanceFieldGlyphCache::DistanceFieldGlyphCache(const Vector2i& sourceSize, con
     #endif
 }
 
+GlyphCacheFeatures DistanceFieldGlyphCache::doFeatures() const {
+    return GlyphCacheFeature::ImageProcessing
+        #ifndef MAGNUM_TARGET_GLES
+        |GlyphCacheFeature::ProcessedImageDownload
+        #endif
+        ;
+}
+
 void DistanceFieldGlyphCache::doSetImage(const Vector2i& offset, const ImageView2D& image) {
     GL::Texture2D input;
     input.setWrapping(GL::SamplerWrapping::ClampToEdge)
         .setMinificationFilter(GL::SamplerFilter::Linear)
         .setMagnificationFilter(GL::SamplerFilter::Linear);
 
-    #ifndef CORRADE_NO_ASSERT
-    const GL::PixelFormat format = GL::pixelFormat(image.format());
-    #endif
-    #if !(defined(MAGNUM_TARGET_GLES) && defined(MAGNUM_TARGET_GLES2))
-    CORRADE_ASSERT(format == GL::PixelFormat::Red,
-        "Text::DistanceFieldGlyphCache::setImage(): expected"
-        << GL::PixelFormat::Red << "but got" << format, );
-    input.setImage(0, GL::TextureFormat::R8, image);
-    #else
+    /* Upload the input texture and create a distance field from it */
+    const Vector2 scale = Vector2{_size}/Vector2{textureSize()};
+
+    /* On ES2 without EXT_unpack_subimage and on WebGL 1 there's no possibility
+       to upload just a slice of the input, upload the whole image instead by
+       ignoring the PixelStorage properties of the input and also process it as
+       a whole. */
+    #ifdef MAGNUM_TARGET_GLES2
     #ifndef MAGNUM_TARGET_WEBGL
-    if(GL::Context::current().isExtensionSupported<GL::Extensions::EXT::texture_rg>()) {
-        CORRADE_ASSERT(format == GL::PixelFormat::Red || format == GL::PixelFormat::Luminance,
-            "Text::DistanceFieldGlyphCache::setImage(): expected"
-            << GL::PixelFormat::Red << "but got" << format, );
-        input.setImage(0, GL::TextureFormat::Red, ImageView2D{image.storage(), GL::PixelFormat::Red, GL::PixelType::UnsignedByte, image.size(), image.data()});
-    } else
+    if(!GL::Context::current().isExtensionSupported<GL::Extensions::EXT::unpack_subimage>())
     #endif
     {
-        CORRADE_ASSERT(format == GL::PixelFormat::Luminance,
-            "Text::DistanceFieldGlyphCache::setImage(): expected"
-            << GL::PixelFormat::Luminance << "but got" << format, );
-        input.setImage(0, GL::TextureFormat::Luminance, image);
+        input.setImage(0, GL::textureFormat(image.format()), ImageView2D{image.format(), size().xy(), image.data()});
+        _distanceField(input, texture(), {{}, size().xy()*scale}, size().xy());
+        #ifdef MAGNUM_TARGET_WEBGL
+        static_cast<void>(offset);
+        #endif
+    }
+    #ifndef MAGNUM_TARGET_WEBGL
+    else
+    #endif
+    #endif
+    #if !(defined(MAGNUM_TARGET_GLES2) && defined(MAGNUM_TARGET_WEBGL))
+    {
+        input.setImage(0, GL::textureFormat(image.format()), image);
+        _distanceField(input, texture(), Range2Di::fromSize(offset*scale, image.size()*scale), image.size());
     }
     #endif
-
-    /* Create distance field from input texture */
-    const Vector2 scale = Vector2{_size}/Vector2{textureSize()};
-    _distanceField(input, texture(), Range2Di::fromSize(offset*scale, image.size()*scale), image.size());
 }
 
 void DistanceFieldGlyphCache::setDistanceFieldImage(const Vector2i& offset, const ImageView2D& image) {
@@ -121,5 +132,12 @@ void DistanceFieldGlyphCache::setDistanceFieldImage(const Vector2i& offset, cons
 
     texture().setSubImage(0, offset, image);
 }
+
+#ifndef MAGNUM_TARGET_GLES
+Image3D DistanceFieldGlyphCache::doProcessedImage() {
+    Image2D out = _texture.image(0, PixelFormat::R8Unorm);
+    return Image3D{out.format(), {out.size(), 1}, out.release()};
+}
+#endif
 
 }}

src/Magnum/Text/Test/DistanceFieldGlyphCacheGLTest.cpp
@@ -28,6 +28,7 @@
 #include <Corrade/Containers/StringStl.h> /**< @todo remove once Debug is stream-free */
 #include <Corrade/PluginManager/AbstractManager.h>
 #include <Corrade/TestSuite/Compare/String.h>
+#include <Corrade/Utility/Algorithms.h>
 #include <Corrade/Utility/DebugStl.h> /**< @todo remove once Debug is stream-free */
 #include <Corrade/Utility/Path.h>
 
@@ -35,6 +36,10 @@
 #include "Magnum/ImageView.h"
 #include "Magnum/PixelFormat.h"
 #include "Magnum/DebugTools/CompareImage.h"
+#ifdef MAGNUM_TARGET_GLES
+#include "Magnum/DebugTools/TextureImage.h"
+#endif
+#include "Magnum/Math/Range.h"
 #include "Magnum/GL/OpenGLTester.h"
 #include "Magnum/GL/Extensions.h"
 #include "Magnum/GL/PixelFormat.h"
@@ -118,27 +123,66 @@ void DistanceFieldGlyphCacheGLTest::setImage() {
     CORRADE_COMPARE(inputImage->size(), (Vector2i{256, 256}));
 
     DistanceFieldGlyphCache cache{data.sourceSize, data.size, 32};
-    /* Test also uploading under an offset */
-    cache.setImage(data.sourceOffset, *inputImage);
+    Containers::StridedArrayView3D<const char> src = inputImage->pixels();
+    /* Test also uploading under an offset. The cache might be three-component
+       in some cases, slice the destination view to just the first component */
+    /** @todo actually the input can be just luminance, only the destination
+        cannot -- fix by dropping the dependency on GlyphCache and creating the
+        texture directly */
+    Utility::copy(src, cache.image().pixels()[0].sliceSize({
+        std::size_t(data.sourceOffset.y()),
+        std::size_t(data.sourceOffset.x()),
+        0}, src.size()));
+    cache.flushImage(Range2Di::fromSize(data.sourceOffset, inputImage->size()));
     MAGNUM_VERIFY_NO_GL_ERROR();
 
+    /* On GLES processedImage() isn't implemented as it'd mean creating a
+       temporary framebuffer. Do it via DebugTools here instead, we cannot
+       really verify that the size matches, but at least something. */
     #ifndef MAGNUM_TARGET_GLES
-    Image2D actual = cache.image();
+    Image3D actual3 = cache.processedImage();
+    /** @todo ugh have slicing on images directly already */
+    MutableImageView2D actual{actual3.format(), actual3.size().xy(), actual3.data()};
+    #else
+    /* Pick a format that matches the internal texture format. This is rather
+       shitty, TBH. */
+    #if !(defined(MAGNUM_TARGET_GLES) && defined(MAGNUM_TARGET_GLES2))
+    const GL::PixelFormat format = GL::PixelFormat::Red;
+    #else
+    GL::PixelFormat format;
+    #ifndef MAGNUM_TARGET_WEBGL
+    if(GL::Context::current().isExtensionSupported<GL::Extensions::EXT::texture_rg>()) {
+        format = GL::PixelFormat::Red;
+    } else
+    #endif
+    {
+        format = GL::PixelFormat::RGB;
+    }
+    #endif
+    Image2D actual = DebugTools::textureSubImage(cache.texture(), 0, {{}, data.size}, {format, GL::PixelType::UnsignedByte});
+    #endif
     MAGNUM_VERIFY_NO_GL_ERROR();
 
     if(!(_manager.loadState("AnyImageImporter") & PluginManager::LoadState::Loaded) ||
        !(_manager.loadState("TgaImporter") & PluginManager::LoadState::Loaded))
         CORRADE_SKIP("AnyImageImporter / TgaImporter plugins not found.");
 
+    /* On GLES2 if EXT_unpack_subimage isn't supported or on WebGL 1, the whole
+       texture gets uploaded and processed every time. Which circumvents this
+       problem. */
+    #if defined(MAGNUM_TARGET_GLES2) && !defined(MAGNUM_TARGET_WEBGL)
+    CORRADE_EXPECT_FAIL_IF(GL::Context::current().isExtensionSupported<GL::Extensions::EXT::unpack_subimage>() && !data.sourceOffset.isZero(),
+        "The distance field tool is currently broken if a non-zero offset is used.");
+    #elif !defined(MAGNUM_TARGET_GLES2)
     CORRADE_EXPECT_FAIL_IF(!data.sourceOffset.isZero(),
         "The distance field tool is currently broken if a non-zero offset is used.");
-    CORRADE_COMPARE_WITH(actual.pixels<UnsignedByte>().exceptPrefix(data.offset),
+    #endif
+    /* The format may be three-component, consider just the first channel */
+    Containers::StridedArrayView3D<const char> pixels = actual.pixels();
+    CORRADE_COMPARE_WITH((Containers::arrayCast<2, const UnsignedByte>(pixels.prefix({pixels.size()[0], pixels.size()[1], 1})).exceptPrefix(data.offset)),
         Utility::Path::join(TEXTURETOOLS_DISTANCEFIELDGLTEST_DIR, "output.tga"),
         /* Same threshold as in TextureTools DistanceFieldGLTest */
         (DebugTools::CompareImageToFile{_manager, 1.0f, 0.178f}));
-    #else
-    CORRADE_SKIP("Skipping image download test as it's not available on GLES.");
-    #endif
 }
 
 void DistanceFieldGlyphCacheGLTest::setDistanceFieldImage() {
@@ -175,8 +219,17 @@ void DistanceFieldGlyphCacheGLTest::setDistanceFieldImage() {
     cache.setDistanceFieldImage({8, 4}, ImageView2D{format, GL::PixelType::UnsignedByte, {8, 4}, data});
     MAGNUM_VERIFY_NO_GL_ERROR();
 
+    /* On GLES processedImage() isn't implemented as it'd mean creating a
+       temporary framebuffer. Do it via DebugTools here instead, we cannot
+       really verify that the size matches, but at least something. */
     #ifndef MAGNUM_TARGET_GLES
-    Image2D actual = cache.image();
+    Image3D actual3 = cache.processedImage();
+    /** @todo ugh have slicing on images directly already */
+    MutableImageView2D actual{actual3.format(), actual3.size().xy(), actual3.data()};
+    #else
+    Image2D actualGL = DebugTools::textureSubImage(cache.texture(), 0, {{}, {16, 8}}, {format, GL::PixelType::UnsignedByte});
+    ImageView2D actual{*GL::genericPixelFormat(format, GL::PixelType::UnsignedByte), actualGL.size(), actualGL.data()};
+    #endif
     MAGNUM_VERIFY_NO_GL_ERROR();
 
     UnsignedByte expected[]{
@@ -192,9 +245,6 @@ void DistanceFieldGlyphCacheGLTest::setDistanceFieldImage() {
     CORRADE_COMPARE_AS(actual,
         (ImageView2D{PixelFormat::R8Unorm, {16, 8}, expected}),
         DebugTools::CompareImage);
-    #else
-    CORRADE_SKIP("Skipping image download test as it's not available on GLES.");
-    #endif
 }
 
 void DistanceFieldGlyphCacheGLTest::setDistanceFieldImageOutOfRange() {

doc/snippets/MagnumText-gl.cpp
@@ -26,6 +26,7 @@
 #include <Corrade/Containers/StringView.h>
 #include <Corrade/PluginManager/Manager.h>
 
+#include "Magnum/PixelFormat.h"
 #include "Magnum/Math/Color.h"
 #include "Magnum/Math/Matrix3.h"
 #include "Magnum/Shaders/VectorGL.h"
@@ -59,6 +60,12 @@ font->fillGlyphCache(cache, "abcdefghijklmnopqrstuvwxyz"
                             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                             "0123456789?!:;,. ");
 /* [AbstractFont-usage] */
+}
+
+{
+/* [AbstractGlyphCache-filling-construct] */
+Text::GlyphCache cache{Vector2i{512}};
+/* [AbstractGlyphCache-filling-construct] */
 }
 
 {

doc/snippets/MagnumText.cpp
@@ -31,17 +31,24 @@
 #include <unordered_map>
 #include <Corrade/Containers/Array.h>
 #include <Corrade/Containers/Optional.h>
+#include <Corrade/Containers/StridedArrayView.h>
 #include <Corrade/Containers/StringView.h>
 #include <Corrade/Containers/StringStl.h> /** @todo remove once file callbacks are <string>-free */
+#include <Corrade/Containers/Triple.h>
 #include <Corrade/PluginManager/Manager.h>
+#include <Corrade/Utility/Algorithms.h>
 #include <Corrade/Utility/Path.h>
 #include <Corrade/Utility/Resource.h>
 
 #include "Magnum/FileCallback.h"
+#include "Magnum/ImageView.h"
 #include "Magnum/Math/Color.h"
 #include "Magnum/Math/Matrix3.h"
+#include "Magnum/Math/Range.h"
 #include "Magnum/Text/AbstractFont.h"
 #include "Magnum/Text/AbstractFontConverter.h"
+#include "Magnum/Text/AbstractGlyphCache.h"
+#include "Magnum/TextureTools/Atlas.h"
 
 #define DOXYGEN_ELLIPSIS(...) __VA_ARGS__
 
@@ -160,6 +167,91 @@ font->setFileCallback([](const std::string& filename,
         return Containers::optional(rs.getRaw(filename));
     }, rs);
 /* [AbstractFont-setFileCallback-template] */
+}
+
+{
+struct: Text::AbstractGlyphCache {
+    using Text::AbstractGlyphCache::AbstractGlyphCache;
+
+    Text::GlyphCacheFeatures doFeatures() const override { return {}; }
+} cache{Vector2i{256}};
+/* [AbstractGlyphCache-filling-images] */
+Containers::ArrayView<const ImageView2D> images = DOXYGEN_ELLIPSIS({});
+/* [AbstractGlyphCache-filling-images] */
+
+/* [AbstractGlyphCache-filling-font] */
+UnsignedInt fontId = cache.addFont(images.size());
+/* [AbstractGlyphCache-filling-font] */
+
+/* [AbstractGlyphCache-filling-atlas] */
+Containers::Array<Vector2i> offsets{NoInit, images.size()};
+
+cache.atlas().clearFlags(
+    TextureTools::AtlasLandfillFlag::RotatePortrait|
+    TextureTools::AtlasLandfillFlag::RotateLandscape);
+CORRADE_INTERNAL_ASSERT(cache.atlas().add(
+    stridedArrayView(images).slice(&ImageView2D::size),
+    offsets));
+/* [AbstractGlyphCache-filling-atlas] */
+
+/* [AbstractGlyphCache-filling-glyphs] */
+/* The glyph cache is just 2D, so copying to the first slice */
+Containers::StridedArrayView3D<char> dst = cache.image().pixels()[0];
+Range2Di updated;
+for(UnsignedInt fontGlyphId = 0; fontGlyphId != images.size(); ++fontGlyphId) {
+    Range2Di rectangle = Range2Di::fromSize(offsets[fontGlyphId],
+                                            images[fontGlyphId].size());
+    cache.addGlyph(fontId, fontGlyphId, {}, rectangle);
+
+    /* Copy assuming all input images have the same pixel format */
+    Containers::StridedArrayView3D<const char> src = images[fontGlyphId].pixels();
+    Utility::copy(src, dst.sliceSize({
+        std::size_t(offsets[fontGlyphId].y()),
+        std::size_t(offsets[fontGlyphId].x()),
+        0}, src.size()));
+
+    /* Maintain a range that was updated in the glyph cache */
+    updated = Math::join(updated, rectangle);
+}
+
+/* Reflect the image data update to the actual GPU-side texture */
+cache.flushImage(updated);
+/* [AbstractGlyphCache-filling-glyphs] */
+}
+
+{
+struct: Text::AbstractGlyphCache {
+    using Text::AbstractGlyphCache::AbstractGlyphCache;
+
+    Text::GlyphCacheFeatures doFeatures() const override { return {}; }
+} cacheInstance{Vector2i{256}};
+/* [AbstractGlyphCache-querying] */
+Containers::Pointer<Text::AbstractFont> font = DOXYGEN_ELLIPSIS({});
+Text::AbstractGlyphCache& cache = DOXYGEN_ELLIPSIS(cacheInstance);
+
+Containers::ArrayView<const UnsignedInt> fontGlyphIds = DOXYGEN_ELLIPSIS({});
+
+Containers::Optional<UnsignedInt> fontId = cache.findFont(font.get());
+DOXYGEN_ELLIPSIS()
+for(std::size_t i = 0; i != fontGlyphIds.size(); ++i) {
+    Containers::Triple<Vector2i, Int, Range2Di> glyph =
+        cache.glyph(*fontId, fontGlyphIds[i]);
+    DOXYGEN_ELLIPSIS(static_cast<void>(glyph);)
+}
+/* [AbstractGlyphCache-querying] */
+
+/* [AbstractGlyphCache-querying-batch] */
+Containers::Array<UnsignedInt> glyphIds{NoInit, fontGlyphIds.size()};
+cache.glyphIdsInto(*fontId, fontGlyphIds, glyphIds);
+
+Containers::StridedArrayView1D<const Vector2i> offsets = cache.glyphOffsets();
+Containers::StridedArrayView1D<const Range2Di> rects = cache.glyphRectangles();
+for(std::size_t i = 0; i != fontGlyphIds.size(); ++i) {
+    Vector2i offset = offsets[glyphIds[i]];
+    Range2Di rectangle = rects[glyphIds[i]];
+    DOXYGEN_ELLIPSIS(static_cast<void>(offset); static_cast<void>(rectangle);)
+}
+/* [AbstractGlyphCache-querying-batch] */
 }
 
 }

src/Magnum/Text/AbstractFont.cpp
@@ -39,6 +39,7 @@
 
 #include "Magnum/FileCallback.h"
 #include "Magnum/Math/Functions.h"
+#include "Magnum/Math/Range.h"
 #include "Magnum/Text/AbstractGlyphCache.h"
 
 #ifndef CORRADE_PLUGINMANAGER_NO_DYNAMIC_PLUGIN_SUPPORT

src/Magnum/Text/AbstractFont.h
@@ -747,7 +747,7 @@ updated interface string.
 */
 /* Silly indentation to make the string appear in pluginInterface() docs */
 #define MAGNUM_TEXT_ABSTRACTFONT_PLUGIN_INTERFACE /* [interface] */ \
-"cz.mosra.magnum.Text.AbstractFont/0.3.2"
+"cz.mosra.magnum.Text.AbstractFont/0.3.3"
 /* [interface] */
 
 #ifndef DOXYGEN_GENERATING_OUTPUT

commit fd2a1d21aef4ff5a1fa9402e374c7a973a6150af

Text: make DistanceFieldGlyphCache processed image upload work on ES2 again.
This was apparently also broken since the refactor in
6707534ce65375a3b79598f6d08f125071ee02c9.

src/Magnum/Text/DistanceFieldGlyphCache.cpp
@@ -168,7 +168,22 @@ void DistanceFieldGlyphCache::setDistanceFieldImage(const Vector2i& offset, cons
 #endif
 
 void DistanceFieldGlyphCache::doSetProcessedImage(const Vector2i& offset, const ImageView2D& image) {
-    texture().setSubImage(0, offset, image);
+    ImageView2D imageToUse = image;
+
+    /* On ES2, R8Unorm maps to Luminance, but here it's actually Red if
+       EXT_texture_rg is supported. Reinterpret the image format in that
+       case. If the format is something else (such as RGBA8Unorm), no
+       reinterpret is done. WebGL doesn't have the EXT_texture_rg extension so
+       there it isn't done either. */
+    #if defined(MAGNUM_TARGET_GLES2) && !defined(MAGNUM_TARGET_WEBGL)
+    if(processedFormat() == PixelFormat::R8Unorm) {
+        /* This is checked inside setProcessedImage() already */
+        CORRADE_INTERNAL_ASSERT(image.format() == PixelFormat::R8Unorm);
+        imageToUse = ImageView2D{image.storage(), GL::PixelFormat::Red, GL::PixelType::UnsignedByte, image.size(), image.data()};
+    }
+    #endif
+
+    texture().setSubImage(0, offset, imageToUse);
 }
 
 #ifndef MAGNUM_TARGET_GLES

src/Magnum/Text/Test/DistanceFieldGlyphCacheGLTest.cpp
@@ -30,6 +30,7 @@
 #include <Corrade/TestSuite/Compare/String.h>
 #include <Corrade/Utility/Algorithms.h>
 #include <Corrade/Utility/DebugStl.h> /**< @todo remove once Debug is stream-free */
+#include <Corrade/Utility/FormatStl.h> /**< @todo remove once Debug is stream-free */
 #include <Corrade/Utility/Path.h>
 
 #include "Magnum/Image.h"
@@ -231,8 +232,18 @@ void DistanceFieldGlyphCacheGLTest::setImage() {
     Image3D actual3 = cache.processedImage();
     /** @todo ugh have slicing on images directly already */
     MutableImageView2D actual{actual3.format(), actual3.size().xy(), actual3.data()};
-    #else
+    #elif !defined(MAGNUM_TARGET_GLES2)
     Image2D actual = DebugTools::textureSubImage(cache.texture(), 0, {{}, data.size}, cache.processedFormat());
+    #else
+    /* On ES2, R8Unorm maps to Luminance, but here it's actually Red if
+       EXT_texture_rg is supported */
+    Image2D actual = DebugTools::textureSubImage(cache.texture(), 0, {{}, data.size},
+        #ifndef MAGNUM_TARGET_WEBGL
+        cache.processedFormat() == PixelFormat::R8Unorm ?
+            Image2D{GL::PixelFormat::Red, GL::PixelType::UnsignedByte} :
+        #endif
+            Image2D{cache.processedFormat()}
+    );
     #endif
     MAGNUM_VERIFY_NO_GL_ERROR();
 
@@ -302,8 +313,20 @@ void DistanceFieldGlyphCacheGLTest::setProcessedImage() {
     Image3D actual3 = cache.processedImage();
     /** @todo ugh have slicing on images directly already */
     MutableImageView2D actual{actual3.format(), actual3.size().xy(), actual3.data()};
-    #else
+    #elif !defined(MAGNUM_TARGET_GLES2)
     Image2D actual = DebugTools::textureSubImage(cache.texture(), 0, {{}, {16, 8}}, cache.processedFormat());
+    #else
+    /* On ES2, R8Unorm maps to Luminance, but here it's actually Red if
+       EXT_texture_rg is supported. We however need the generic format again
+       below for comparison, so reinterpret it back. */
+    Image2D actualGLFormat = DebugTools::textureSubImage(cache.texture(), 0, {{}, {16, 8}},
+        #ifndef MAGNUM_TARGET_WEBGL
+        cache.processedFormat() == PixelFormat::R8Unorm ?
+            Image2D{GL::PixelFormat::Red, GL::PixelType::UnsignedByte} :
+        #endif
+            Image2D{cache.processedFormat()}
+    );
+    ImageView2D actual{actualGLFormat.storage(), PixelFormat::R8Unorm, actualGLFormat.size(), actualGLFormat.data()};
     #endif
     MAGNUM_VERIFY_NO_GL_ERROR();
 
@@ -337,9 +360,10 @@ void DistanceFieldGlyphCacheGLTest::setDistanceFieldImageUnsupportedGLFormat() {
     /* Format that doesn't have a generic equivalent gets passed as-is */
     cache.setDistanceFieldImage({}, ImageView2D{GL::PixelFormat::RGBA, GL::PixelType::UnsignedShort5551, {1, 1}, "hello!!"});
     CORRADE_IGNORE_DEPRECATED_POP
-    CORRADE_COMPARE_AS(out.str(),
-        "Text::AbstractGlyphCache::setProcessedImage(): expected PixelFormat::R8Unorm but got PixelFormat::RGBA32F\n"
-        "Text::AbstractGlyphCache::setProcessedImage(): expected PixelFormat::R8Unorm but got PixelFormat::ImplementationSpecific(0x1908)\n",
+    CORRADE_COMPARE_AS(out.str(), Utility::formatString(
+        "Text::AbstractGlyphCache::setProcessedImage(): expected PixelFormat::{0} but got PixelFormat::RGBA32F\n"
+        "Text::AbstractGlyphCache::setProcessedImage(): expected PixelFormat::{0} but got PixelFormat::ImplementationSpecific(0x1908)\n",
+        cache.processedFormat() == PixelFormat::RGBA8Unorm ? "RGBA8Unorm" : "R8Unorm"),
         TestSuite::Compare::String);
 }
 #endif