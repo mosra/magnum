ef133cbaddba39154453d4dbade3b4a9eebb8d58
Vladimír Vondruš <mosra@centrum.cz>
2024-10-30 10:58:10 +0100

    package/rpm: go back to the package name being a variable.
    The script is exactly the same across repos, this makes it easier to
    update in bulk.

--- package/rpm/build.sh
+++ package/rpm/build.sh
@@ -32,6 +32,7 @@
 set -e
 
 # Get version slug
+package_name=magnum
 version_hash=$(git describe --match "v*" | sed 's/^v//' | sed 's/-/./g')
 echo "** repository hash: ${version_hash} ..."
 
@@ -39,19 +40,19 @@ echo "** repository hash: ${version_hash} ..."
 rpmdev-setuptree
 
 # Archive repository
-(cd ../.. && git archive --format=tar.gz --prefix=magnum-${version_hash}/ -o ~/rpmbuild/SOURCES/magnum-${version_hash}.tar.gz HEAD)
-echo "** created archive: ~/rpmbuild/SOURCES/magnum-${version_hash}.tar.gz"
+(cd ../.. && git archive --format=tar.gz --prefix=${package_name}-${version_hash}/ -o ~/rpmbuild/SOURCES/${package_name}-${version_hash}.tar.gz HEAD)
+echo "** created archive: ~/rpmbuild/SOURCES/${package_name}-${version_hash}.tar.gz"
 sleep 2
 
 # Replace spec version
-sed -i "s/Version:.\+/Version: ${version_hash}/g" magnum.spec
+sed -i "s/Version:.\+/Version: ${version_hash}/g" ${package_name}.spec
 echo "** building package version: ${version_hash}"
 
 # Check dependencies
-sudo dnf builddep -y magnum.spec
+sudo dnf builddep -y ${package_name}.spec
 
 # Build package
-rpmbuild --define "debug_package %{nil}" --clean -bb magnum.spec
+rpmbuild --define "debug_package %{nil}" --clean -bb ${package_name}.spec
 
-echo "** packages for magnum-${version_hash} complete:"
-ls ~/rpmbuild/RPMS/$(uname -m)/magnum-${version_hash}*.rpm | cat
+echo "** packages for ${package_name}-${version_hash} complete:"
+ls ~/rpmbuild/RPMS/$(uname -m)/${package_name}-*${version_hash}*.rpm | cat
