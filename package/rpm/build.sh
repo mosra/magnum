654ae9d2fb82932fd8f745dfe73cc97e32817296
Vladimír Vondruš <mosra@centrum.cz>
2024-10-30 10:51:46 +0100

    package/rpm: wrong license header copypaste.

--- package/rpm/build.sh
+++ package/rpm/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #
-#   This file is part of Corrade.
+#   This file is part of Magnum.
 #
-#   Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
-#               2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024
+#   Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
+#               2020, 2021, 2022, 2023, 2024
 #             Vladimír Vondruš <mosra@centrum.cz>
 #   Copyright © 2018, 2020, 2021, 2022, 2023, 2024
 #             Igal Alkon <igal.alkon@gmail.com>
