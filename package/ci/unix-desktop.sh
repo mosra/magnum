15ae591b0e4036030da9f0cd5266b5456aee7b6e
Vladimír Vondruš <mosra@centrum.cz>
2025-07-08 16:31:08 +0200

    package/ci: GL_ARB_robustness was disabled in two runs by accident.
    The PKGBUILD-coverage had it correct.

--- package/ci/unix-desktop.sh
+++ package/ci/unix-desktop.sh
@@ -82,7 +82,7 @@ export TSAN_OPTIONS="color=always"
 if [ "$TARGET_EGL" == "ON" ]; then
     # Keep in sync with package/archlinux/PKGBUILD and PKGBUILD-coverage
     ctest -V -E "GLBenchmark|VkTest"
-    MAGNUM_DISABLE_EXTENSIONS="GL_ARB_invalidate_subdata GL_ARB_multi_bind GL_ARB_robustness GL_ARB_separate_shader_objects GL_ARB_texture_storage GL_ARB_texture_storage_multisample GL_ARB_shading_language_420pack GL_ARB_explicit_uniform_location GL_ARB_explicit_attrib_location GL_ARB_texture_filter_anisotropic" ctest -V -R GLTest
+    MAGNUM_DISABLE_EXTENSIONS="GL_ARB_invalidate_subdata GL_ARB_multi_bind GL_ARB_separate_shader_objects GL_ARB_texture_storage GL_ARB_texture_storage_multisample GL_ARB_shading_language_420pack GL_ARB_explicit_uniform_location GL_ARB_explicit_attrib_location GL_ARB_texture_filter_anisotropic" ctest -V -R GLTest
     MAGNUM_DISABLE_EXTENSIONS="GL_ARB_direct_state_access GL_ARB_get_texture_sub_image GL_ARB_robustness" ctest -V -R GLTest
     MAGNUM_DISABLE_EXTENSIONS="GL_ARB_uniform_buffer_object GL_ARB_shader_storage_buffer_object GL_ARB_vertex_array_object GL_KHR_debug" ctest -V -R GLTest
     $CONFIGURATION/bin/magnum-gl-info --limits
