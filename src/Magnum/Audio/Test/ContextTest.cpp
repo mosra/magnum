ea060cd9d6a88ec9b41a4e18f6748988b5b58568
Vladimír Vondruš <mosra@centrum.cz>
2026-01-01 23:38:58 +0100

    Update copyright year.

--- src/Magnum/Audio/Test/ContextTest.cpp
+++ src/Magnum/Audio/Test/ContextTest.cpp
@@ -2,7 +2,7 @@
     This file is part of Magnum.
 
     Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
-                2020, 2021, 2022, 2023, 2024, 2025
+                2020, 2021, 2022, 2023, 2024, 2025, 2026
               Vladimír Vondruš <mosra@centrum.cz>
     Copyright © 2015 Jonathan Hale <squareys@googlemail.com>
 
